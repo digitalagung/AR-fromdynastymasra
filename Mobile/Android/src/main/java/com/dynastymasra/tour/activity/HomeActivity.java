758414205949558091510584c145bfd0c31d9bb3
dynastymasra <dynastymasra@gmail.com>
2015-03-31 16:33:42 +0700

    add home page

---
+++ Mobile/Android/src/main/java/com/dynastymasra/tour/activity/HomeActivity.java
@@ -0,0 +1,20 @@
+package com.dynastymasra.tour.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.dynastymasra.tour.R;
+
+/*
+ * Author   : @dynastymasra
+ * User     : Dimas Ragil T
+ * Email    : dynastymasra@gmail.com
+ * LinkedIn : http://www.linkedin.com/in/dynastymasra
+ * Blogspot : dynastymasra.wordpress.com | dynastymasra.blogspot.com
+*/
+public class HomeActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
